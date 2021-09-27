68305a0b5b345ce5f439d43b1bd3250af8ddd341
Adolfo Yakov Castañeda Navarrete <acastaneda@zapopan.gob.mx>
2021-09-26 19:52:19 -0500

    CONFIGURACIÓN PARA HEROKU

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
-var path = require("path")
+mongoose.connect(   
+  'mongodb+srv://Equipo16:equipo16@cluster0.mltwg.mongodb.net/EmbalajesApi'
+);
+mongoose.set('debug', true);
+
+//var path = require("path")
+
 require('./models/Cliente');
 require('./models/Venta');
 require('./models/vendedor');
@@ -10,17 +19,10 @@ require('./models/Producto');
 app.use('/team16', require('./routes'))
 
 //Bodyparser es como se parsea el cuerpo
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-mongoose.connect(
-      process.env.MONGO_URI, // obtiene la url de conexión desde las variables de entorno
-     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true } 
-    //'mongodb+srv://Equipo16:equipo16@cluster0.mltwg.mongodb.net/EmbalajesApi'
-);
-mongoose.set('debug', true);
-app.use("/controllers",express.static(path.join(__dirname,"controllers") ) )
+
+//app.use("/controllers",express.static(path.join(__dirname,"controllers") ) )
 //Como manejar las rutas
-app.use(express.static(path.join(__dirname, '/')));
+//app.use(express.static(path.join(__dirname, '/')));
 
 
 //indicamos que pagina saldra al iniciar
@@ -31,9 +33,9 @@ app.use(express.static(path.join(__dirname, '/')));
 
 //Iniciando el servidor
 
-//const PORT = 4001;
+const PORT = 4001;
 
-app.listen(process.env.PORT, () => {
-	console.log(`Server listening on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
 })
 
