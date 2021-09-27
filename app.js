25c6821490f33c8ec3af99a6c671208c4be839d7
Adolfo Yakov Castañeda Navarrete <acastaneda@zapopan.gob.mx>
2021-09-26 20:25:57 -0500

    CONFIGURACIÓN PARA HEROKU

--- app.js
+++ app.js
@@ -5,38 +5,37 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect(   
+mongoose.connect(
+   
   'mongodb+srv://Equipo16:equipo16@cluster0.mltwg.mongodb.net/EmbalajesApi'
 );
 mongoose.set('debug', true);
 
-//var path = require("path")
-
+var path = require("path")
 require('./models/Cliente');
 require('./models/Venta');
 require('./models/vendedor');
 require('./models/Producto');
-app.use('/v1', require('./routes'))
+app.use('/team16', require('./routes'))
 
 //Bodyparser es como se parsea el cuerpo
 
-//app.use("/controllers",express.static(path.join(__dirname,"controllers") ) )
+app.use("/controllers",express.static(path.join(__dirname,"controllers") ) )
 //Como manejar las rutas
-//app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '/')));
 
 
 //indicamos que pagina saldra al iniciar
-/*app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"Pages/index.html"))
 })
-*/
+
 
 //Iniciando el servidor
 
-const PORT = 4001;
-/*
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`)
+//const PORT = 4001;
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`)
 })
 
-*/
