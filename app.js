25b2bf00b4a8bb9e17fd6f776d66e9631b48203d
Adolfo Yakov Castañeda Navarrete <acastaneda@zapopan.gob.mx>
2021-09-26 19:38:21 -0500

    CONFIGURACIÓN PARA HEROKU

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ require('./models/Cliente');
 require('./models/Venta');
 require('./models/vendedor');
 require('./models/Producto');
-
-
 app.use('/team16', require('./routes'))
+
 //Bodyparser es como se parsea el cuerpo
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 mongoose.connect(
-    'mongodb+srv://Equipo16:equipo16@cluster0.mltwg.mongodb.net/EmbalajesApi'
+      process.env.MONGO_URI, // obtiene la url de conexión desde las variables de entorno
+     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true } 
+    //'mongodb+srv://Equipo16:equipo16@cluster0.mltwg.mongodb.net/EmbalajesApi'
 );
 mongoose.set('debug', true);
 app.use("/controllers",express.static(path.join(__dirname,"controllers") ) )
@@ -30,10 +31,9 @@ app.get("/",(req,res)=>{
 
 //Iniciando el servidor
 
-
-mongoose.set('debug', true);
-app.use('/v1', require('./routes'));  
 const PORT = 4001;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`)
+})
+
