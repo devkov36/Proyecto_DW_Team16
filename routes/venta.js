c742251c7609673eca3ebbfa4923bb11c8036478
Adolfo Yakov Castañeda Navarrete <acastaneda@zapopan.gob.mx>
2021-09-26 20:32:13 -0500

    CONFIGURACIÓN PARA HEROKU- CORRECCIÓN VENDEDOR

--- routes/venta.js
+++ routes/venta.js
@@ -4,7 +4,6 @@ const { crearVenta,
     obtenerVenta, 
     modificarVenta, 
     eliminarVenta,
-<<<<<<< HEAD
     busquedaVentaFolio,
     busquedaClienteNombre
 } = require('../controllers/venta');
@@ -12,13 +11,6 @@ const { crearVenta,
 router.get('/:id', obtenerVenta);
 router.get('/sbf/:fol', busquedaVentaFolio);
 router.get('/nmb/:nombre', busquedaClienteNombre);
-=======
-    busquedaVentaFolio
-} = require('../controllers/venta');
-
-router.get('/:id', obtenerVenta);
-router.get('/sbf/:folio', busquedaVentaFolio);
->>>>>>> 639d262480034a0a659e353cda261695c612b107
 router.get('/', obtenerVenta);
 router.post('/', crearVenta);
 router.put('/:id', modificarVenta);
